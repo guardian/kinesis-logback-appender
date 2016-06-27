aafaa17761e84c00c88a8853d614d4bc8170af34
Fabio Crisci <piuccio@gmail.com>
2016-06-27 15:04:38 +0100

    update dependencies (#6)

--- src/main/java/com/gu/logback/appender/kinesis/AppenderConstants.java
+++ src/main/java/com/gu/logback/appender/kinesis/AppenderConstants.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
  * A copy of the License is located at
- * 
+ *
  *  http://aws.amazon.com/apache2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing
@@ -20,7 +20,7 @@ import com.amazonaws.regions.Regions;
  * Contains constants and default configuration values for the appender
  */
 public class AppenderConstants {
-  public static final String USER_AGENT_STRING = "kinesis-logback-appender/1.2.0";
+  public static final String USER_AGENT_STRING = "kinesis-logback-appender/1.3.0";
   // Default values
   public static final String DEFAULT_ENCODING = "UTF-8";
   public static final int DEFAULT_MAX_RETRY_COUNT = 3;
