76d7afae4c3d9d3c90d8d1f8fd765d05de30e031
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-05-15 13:39:35 +0100

    Remove new json lib in favour of Jackson

--- src/main/java/com/gu/logback/appender/kinesis/JsonKinesisAppender.java
+++ src/main/java/com/gu/logback/appender/kinesis/JsonKinesisAppender.java
@@ -1,10 +1,9 @@
 package com.gu.logback.appender.kinesis;
 
 import ch.qos.logback.core.spi.DeferredProcessingAware;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Iterator;
-import java.util.Map;
-import javax.json.*;
+import java.util.*;
 
 public class JsonKinesisAppender <Event extends DeferredProcessingAware>
         extends KinesisAppender<Event> {
@@ -19,15 +18,10 @@ public class JsonKinesisAppender <Event extends DeferredProcessingAware>
     @Override
     protected void putMessage(String message) throws Exception {
         // build a json object
-        JsonObjectBuilder json = Json.createObjectBuilder();
-        Iterator i = standardTags.keySet().iterator();
-        while (i.hasNext()) {
-            String k = (String) i.next();
-            String v = standardTags.get(k);
-            json.add(k, v);
-        }
-        json.add("message", message);
-        super.putMessage(json.build().toString());
+        Map<String,String> payload = new HashMap<String, String>();
+        payload.putAll(standardTags);
+        payload.put("message", message);
+        super.putMessage(new ObjectMapper().writeValueAsString(payload));
     }
 
 }
